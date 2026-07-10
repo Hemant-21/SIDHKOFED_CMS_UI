f6f4e566ff5274f3a11e3a8f8238aa51af3be377
Hemant-21 <hemantsada46@gmail.com>
2026-07-10 12:16:14 +0530

    Updated from V1

--- src/components/form/use-zod-form.ts
+++ src/components/form/use-zod-form.ts
@@ -15,6 +15,7 @@ export function useZodForm<TValues extends FieldValues>(
   options?: Omit<UseFormProps<TValues>, 'resolver'>,
 ): UseFormReturn<TValues> {
   return useForm<TValues>({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     resolver: zodResolver(schema as any),
     mode: 'onTouched',
     ...options,
